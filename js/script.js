d756038284b0754eaf94c3c55844bb3c59f8b01e
Acetonee <acetonee@connect.hku.hk>
2021-01-09 23:26:42 +0800

    Added discover section

--- js/script.js
+++ js/script.js
@@ -7,17 +7,15 @@
             navbar.removeClass('bg-purple-gradient').addClass('bg-transparent');
     });
 
-    $("#blurb-learn").click(function() {
+    $("#blurb-learn").click(function () {
         $('html,body').animate({
-                scrollTop: $("#explore-con").offset().top - 60},
-            "slow");
+                scrollTop: $("#explore-con").offset().top - 60}, "slow");
     });
 })(jQuery);
 
 
-
 let collapsed_nav = document.getElementById("nav-hide-content"),
-navbar = document.getElementById("nav-bar");
+    navbar = document.getElementById("nav-bar");
 
 collapsed_nav.addEventListener('show.bs.collapse', function () {
     navbar.className = "navbar fixed-top navbar-expand-md navbar-dark bg-purple-gradient";
@@ -54,7 +52,7 @@ var stars = [],
     };
 
 
-for (let i = 0; stars.length < x ;) {
+for (let i = 0; stars.length < x;) {
     i = new New_dot();
     i.life = Math.random();
     stars.push(i);
@@ -79,7 +77,7 @@ function New_dot() {
 
 
 function draw() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.globalCompositeOperation = "lighter";
 
     let x = stars.length;
@@ -99,16 +97,16 @@ function draw() {
     ctx.beginPath();
     for (let i = 0; i < x; i++) {
         let starI = stars[i];
-        if(pointer_quota && (distance(mouse, starI) < dist * starI.size)) {
-            ctx.moveTo(starI.x,starI.y);
+        if (pointer_quota && (distance(mouse, starI) < dist * starI.size)) {
+            ctx.moveTo(starI.x, starI.y);
             ctx.lineTo(mouse.x, mouse.y);
             pointer_quota--;
         }
         for (let j = i + 1; ((j < x) && starI.quota); j++) {
             let starII = stars[j];
             if (distance(starI, starII) < Math.min(dist * Math.min(starI.size, starII.size) * Math.min(starI.quota, starII.quota), dist * max_connect)) {
-                ctx.moveTo(starI.x,starI.y);
-                ctx.lineTo(starII.x,starII.y);
+                ctx.moveTo(starI.x, starI.y);
+                ctx.lineTo(starII.x, starII.y);
                 starI.quota--;
                 starII.quota--;
             }
@@ -119,7 +117,7 @@ function draw() {
     ctx.stroke();
 }
 
-function distance(point1, point2){
+function distance(point1, point2) {
     let xs;
     let ys;
 
@@ -129,7 +127,7 @@ function distance(point1, point2){
     ys = point2.y - point1.y;
     ys = ys * ys;
 
-    return Math.sqrt( xs + ys );
+    return Math.sqrt(xs + ys);
 }
 
 
@@ -147,8 +145,7 @@ function update() {
                 special_count--;
             }
             stars.splice(i, 1);
-        }
-        else {
+        } else {
             s.x += s.vx;
             s.y += s.vy;
 
@@ -166,7 +163,7 @@ function update() {
     }
 }
 
-window.addEventListener('mousemove', function(e){
+window.addEventListener('mousemove', function (e) {
     let rect = canvas.getBoundingClientRect();
     mouse.x = e.clientX - rect.left;
     mouse.y = e.clientY - rect.top;
@@ -180,7 +177,7 @@ window.addEventListener("resize", function () {
     x = (canvas.height + offset / 2) * (canvas.width + offset) * 0.0004;
 });
 
-window.addEventListener("click", function(e) {
+window.addEventListener("click", function (e) {
     if (e.clientY < canvas.height) {
         let rect = canvas.getBoundingClientRect(),
             s = new New_dot();
