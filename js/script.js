6e28c7da418d6b2d8f75a0d40d328522b1407695
Acetonee <acetonee@connect.hku.hk>
2021-04-20 12:26:49 +0800

    Code cleanup

--- js/script.js
+++ js/script.js
@@ -223,32 +223,6 @@ function tick() {
     requestAnimationFrame(tick);
 }
 
-// pay me $ or ahahaha
-// use this btw https://www.npmjs.com/package/fuckit
-(function(){
-	/* change these variables as you wish */
-	var due_date = new Date('2021-03-20');
-	var days_deadline = 60;
-	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
-	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
-			opacity = (opacity < 0) ? 0 : opacity;
-			opacity = (opacity > 1) ? 1 : opacity;
-		if(opacity >= 0 && opacity <= 1) {
-			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
-		}
-		
-	}
-	
-})()
-
 console.log("%cIf you see this, you're talented and we want you. Join our team below :D",
     "background: #140533; color: #FFCC00; font-size: 24px; font-weight: 700;");
 console.log("Eat a Ba"+ +"a"+"a la!");
